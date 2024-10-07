41dabb235f80c68a7bc7e19587d4577b46a4b78f
izarofserrano <i.serrano@opendeusto.es>
2024-10-07 16:46:16 +0200

    Otro commit

--- RevistaModa/src/revistaModa/src/main/Main.java
+++ RevistaModa/src/revistaModa/src/main/Main.java
@@ -3,7 +3,7 @@ package revistaModa.src.main;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("He borrado todo");
+		System.out.println("He borrado todo.");
 	}
 
 }
