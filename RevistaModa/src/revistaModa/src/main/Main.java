5837c3798a4f9db99c1f503a75ea5739df7b68da
izarofserrano <i.serrano@opendeusto.es>
2024-10-07 16:45:36 +0200

    Otro commit

--- RevistaModa/src/revistaModa/src/main/Main.java
+++ RevistaModa/src/revistaModa/src/main/Main.java
@@ -3,12 +3,7 @@ package revistaModa.src.main;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world");
-		System.out.println("Hola Mundo");
-		System.out.println("Hola esto es una prueba");
-		System.out.println("hola");
-		System.out.println("hola hola");
-		System.out.println("hola prueba");
+		System.out.println("He borrado todo");
 	}
 
 }
