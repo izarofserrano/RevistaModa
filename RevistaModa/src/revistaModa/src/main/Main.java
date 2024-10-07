2d270c9fbbe8e654020f15e9464132523ea333a0
genesis balcazar <g.balcazar@opendeusto.es>
2024-10-07 16:27:29 +0200

    prueba de escritura

--- RevistaModa/src/revistaModa/src/main/Main.java
+++ RevistaModa/src/revistaModa/src/main/Main.java
@@ -5,6 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 		System.out.println("Hola Mundo");
+		System.out.println("Hola esto es una prueba");
 
 	}
 
