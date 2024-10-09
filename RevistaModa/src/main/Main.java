7c51c95afadc189680fefc3b270c29b1401551dc
izarofserrano <i.serrano@opendeusto.es>
2024-10-09 16:26:48 +0200

    Cambio de token a ssh

--- RevistaModa/src/main/Main.java
+++ RevistaModa/src/main/Main.java
@@ -5,6 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("He borrado todo.");
 		System.out.println("Nuevo commit");
+		System.out.println("Quiero probar el ssh2");
 	}
 
 }
