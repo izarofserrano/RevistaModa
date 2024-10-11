b0ee0ed0447e1e02dcd36c43c813d33f44b1ba92
ainhoaezhz <a.ezkurdia@opendeusto.es>
2024-10-11 16:28:56 +0200

    Imagen de portada añadida y footer con información

--- RevistaModa/src/revistaModa/gui/VentanaInicial.java
+++ RevistaModa/src/revistaModa/gui/VentanaInicial.java
@@ -2,6 +2,7 @@ package revistaModa.gui;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.GridLayout;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -15,7 +16,7 @@ import javax.swing.JPanel;
 
 public class VentanaInicial extends JFrame {
 	private JButton btnInicio, btnModa, btnBelleza, btnLogIn, btnBuscador;
-	private JLabel lblTitulo;
+	private JLabel lblTitulo, lblImagenPortada, lblContacto, lblUbi;
 	private JPanel pCentro, pNorte, pSur, pEste, pOeste;
 
 	
@@ -25,7 +26,6 @@ public class VentanaInicial extends JFrame {
 		vActual = this;
 		
 		setBounds(100, 100, 1000, 600);
-		
 		setTitle("VOGUE");
 		
 		pCentro = new JPanel();
@@ -44,25 +44,39 @@ public class VentanaInicial extends JFrame {
 		btnModa = new JButton("MODA");
 		btnBelleza = new JButton("BELLEZA");
 		btnLogIn = new JButton("Log In");
+		btnBuscador = new JButton("Buscar");
 		
 		Dimension buttonSize = new Dimension(100, 40);
 		btnInicio.setPreferredSize(buttonSize);
 		btnModa.setPreferredSize(buttonSize);
 		btnBelleza.setPreferredSize(buttonSize);
 		btnLogIn.setPreferredSize(buttonSize);
+		btnBuscador.setPreferredSize(buttonSize);
 		
-		btnBuscador = new JButton();
-		btnBuscador.setPreferredSize(new Dimension(40,40));
-		btnBuscador.setToolTipText("Buscar");
-		
-		ImageIcon imagen = new ImageIcon("img/lupa.png");
-		setIconImage(imagen.getImage());
+		pSur.setLayout(new GridLayout(10,30));
+		JLabel lblContacto = new JLabel("Contacto: a.ezkurdia@opendeusto.es");
+		JLabel lblUbi = new JLabel("Ubicación: Universidad de Deusto, Bilbao, Bizkaia, España");
 		
 		pNorte.add(btnInicio);
 		pNorte.add(btnModa);
 		pNorte.add(btnBelleza);
 		pNorte.add(btnLogIn);
 		pNorte.add(btnBuscador);
+		
+		pSur.add(lblContacto);
+		pSur.add(lblUbi);
+		
+		try {
+			ImageIcon iconoPortada = new ImageIcon("img/portada.jpeg");
+			Image imgPortada = iconoPortada.getImage().getScaledInstance(getWidth(), 600, Image.SCALE_SMOOTH);
+			lblImagenPortada = new JLabel(new ImageIcon(imgPortada));
+			pCentro.add(lblImagenPortada);
+		
+		} catch (Exception e) {
+			System.out.println("No se ha podido cargar la imagen" + e.getMessage());
+		}
+		
+		
 		
 		
 		btnLogIn.addActionListener(new ActionListener() {
@@ -79,4 +93,5 @@ public class VentanaInicial extends JFrame {
 		
 		setVisible(true);
 	}
+	
 }
