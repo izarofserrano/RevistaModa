3a5690aedb00f1e985897df01e9d62d650899df7
genesis balcazar <g.balcazar@opendeusto.es>
2024-10-11 16:46:36 +0200

    Añadimos componentes a la ventana

--- RevistaModa/src/revistaModa/gui/VentanaUsuario.java
+++ RevistaModa/src/revistaModa/gui/VentanaUsuario.java
@@ -1,12 +1,19 @@
 package revistaModa.gui;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JPasswordField;
 import javax.swing.JTextField;
+import javax.swing.SwingConstants;
 
 public class VentanaUsuario extends JFrame {
 
@@ -29,14 +36,62 @@ public class VentanaUsuario extends JFrame {
 		getContentPane().add(pNorte,BorderLayout.NORTH);
 		
 		
-		JButton btnEntrar = new JButton("Entrar");
-
-		JTextField userName = new JTextField();
-		JTextField contra = new JTextField();
 		
-		pEste.add(userName);
-		pEste.add(contra);
-		pEste.add(btnEntrar);
+		JTextField userName = new JTextField();
+		userName.setSize(new Dimension(300,75));
+		userName.setColumns(40);
+		
+		JLabel Usuario = new JLabel("Usuario: ");
+		Usuario.setVerticalAlignment(SwingConstants.EAST);
+		Usuario.setFont(new Font("Arial", Font.BOLD, 18));
+		
+		JPanel  p1 = new JPanel();
+		p1.add(Usuario);
+		p1.add(userName);
+		pEste.add(p1);
+        
+	
+		
+		JPasswordField contra = new JPasswordField();
+		contra.setSize(new Dimension(300,75));
+		contra.setColumns(40);
+		
+		JLabel password = new JLabel("Contraseña: ");
+		password.setVerticalAlignment(SwingConstants.EAST);
+		password.setFont(new Font("Arial", Font.BOLD, 18));
+		
+		JPanel  p2 = new JPanel();
+		p2.add(password);
+		p2.add(contra);
+		pEste.add(p2);
+		
+		
+		JButton btnEntrar = new JButton("Entrar");
+		JPanel p3 = new JPanel();
+		p3.add(btnEntrar);
+		pEste.add(p3);
+		
+		
+		JLabel title = new JLabel("Title");
+		title.setFont(new Font("Arial", Font.BOLD, 50));
+		pNorte.add(title);
+		JPanel p4 = new JPanel();
+		p4.add(title);
+		pNorte.add(p4);
+		
+		JButton btnAtras= new JButton("Atrás");
+		btnAtras.setSize(new Dimension(300,300));
+		btnAtras.setAlignmentX(SwingConstants.WEST);
+		//lamda
+		btnAtras.addActionListener((e)->{
+			new VentanaInicial();
+		});
+		JPanel p5 = new JPanel();
+		p5.add(btnAtras);
+		pNorte.add(p5);
+		
+		
+		
 		
 		setVisible(true);
 	}
