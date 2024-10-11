f1ad8461074a08306a43932da0742ab70e355636
ainhoaezhz <a.ezkurdia@opendeusto.es>
2024-10-11 15:48:16 +0200

    Ventana inicial

---
+++ RevistaModa/src/revistaModa/gui/VentanaInicial.java
@@ -0,0 +1,86 @@
+package revistaModa.gui;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+
+public class VentanaInicial extends JFrame {
+	private JButton btnInicio, btnModa, btnBelleza, btnLogIn, btnBuscador;
+	private JLabel lblTitulo;
+	private JPanel pCentro, pNorte, pSur, pEste, pOeste;
+
+	
+	private JFrame vActual;
+	
+	public VentanaInicial() {
+		vActual = this;
+		
+		setBounds(100, 100, 1000, 600);
+		
+		setTitle("VOGUE");
+		
+		pCentro = new JPanel();
+		pNorte = new JPanel();
+		pSur = new JPanel();
+		pEste = new JPanel();
+		pOeste = new JPanel();
+		
+		getContentPane().add(pNorte, BorderLayout.NORTH);
+		getContentPane().add(pSur, BorderLayout.SOUTH);
+		getContentPane().add(pEste, BorderLayout.EAST);
+		getContentPane().add(pOeste, BorderLayout.WEST);
+		getContentPane().add(pCentro, BorderLayout.CENTER);
+		
+		btnInicio = new JButton("INICIO");
+		btnModa = new JButton("MODA");
+		btnBelleza = new JButton("BELLEZA");
+		btnLogIn = new JButton("Log In");
+		
+		Dimension buttonSize = new Dimension(100, 40);
+		btnInicio.setPreferredSize(buttonSize);
+		btnModa.setPreferredSize(buttonSize);
+		btnBelleza.setPreferredSize(buttonSize);
+		btnLogIn.setPreferredSize(buttonSize);
+		
+		btnBuscador = new JButton();
+		btnBuscador.setPreferredSize(new Dimension(40,40));
+		btnBuscador.setToolTipText("Buscar");
+		
+		ImageIcon imagen = new ImageIcon("img/lupa.png");
+		setIconImage(imagen.getImage());
+		
+		/*
+		try {
+			ImageIcon iconoLupa = new ImageIcon("img/lupa.png");
+			Image imgLupa = iconoLupa.getImage().getScaledInstance(20, 20, Image.SCALE_SMOOTH);
+			btnBuscador.setIcon(new ImageIcon(imgLupa));
+		} catch (Exception e){
+			System.out.println("No se pudo cargar la imagen: ");
+		} */
+		
+		btnBuscador = new JButton(new ImageIcon("/Users/ainhoa/Desktop"));
+		btnBuscador.setToolTipText("Buscar");
+		
+		pNorte.add(btnInicio);
+		pNorte.add(btnModa);
+		pNorte.add(btnBelleza);
+		pNorte.add(btnLogIn);
+		pNorte.add(btnBuscador);
+		
+		
+		
+		setVisible(true);
+	}
+	
+	public static void main(String[] args) {
+		VentanaInicial v = new VentanaInicial();
+	}
+
+}
