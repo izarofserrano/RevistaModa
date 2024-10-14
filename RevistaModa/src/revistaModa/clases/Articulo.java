0f4471e855d1fcc1c36301f24dabec203664379f
izarofserrano <i.serrano@opendeusto.es>
2024-10-14 15:42:02 +0200

    Modificación de los atributos de las clases

--- RevistaModa/src/revistaModa/clases/Articulo.java
+++ RevistaModa/src/revistaModa/clases/Articulo.java
@@ -1,81 +1,120 @@
 package revistaModa.clases;
 
-import java.sql.Date;
 
 public class Articulo {
 	
-	private static int contadorId = 1;
-	private int id;
+	private int idArt;
 	private String titulo;
 	private String autor;
-	private Date fechaPublicacion;
-	private TipoArticulo tipoArt;
+	private String fechaPublicacion;
+	private String tipoArt;
+	private int cantLikes;
 	private String rutaArchivoArt;
-	
-	public Articulo(int id, String titulo, String autor, Date fechaPublicacion, TipoArticulo tipoArt,
-			String rutaArchivoArt) {
+	private String rutaFotoPerfil;
+
+
+	public Articulo(int idArt, String titulo, String autor, String fechaPublicacion, String tipoArt, int cantLikes,
+			String rutaArchivoArt, String rutaFotoPerfil) {
 		super();
-		this.id = Articulo.contadorId++;  
+		this.idArt = idArt;
 		this.titulo = titulo;
 		this.autor = autor;
 		this.fechaPublicacion = fechaPublicacion;
 		this.tipoArt = tipoArt;
+		this.cantLikes = cantLikes;
 		this.rutaArchivoArt = rutaArchivoArt;
+		this.rutaFotoPerfil = rutaFotoPerfil;
 	}
 
-	public int getId() {
-		return id;
+
+	public int getIdArt() {
+		return idArt;
 	}
 
-	public void setId(int id) {
-		this.id = id;
+
+	public void setIdArt(int idArt) {
+		this.idArt = idArt;
 	}
+
 
 	public String getTitulo() {
 		return titulo;
 	}
 
+
 	public void setTitulo(String titulo) {
 		this.titulo = titulo;
 	}
+
 
 	public String getAutor() {
 		return autor;
 	}
 
+
 	public void setAutor(String autor) {
 		this.autor = autor;
 	}
 
-	
-	public Date getFechaPublicacion() {
+
+	public String getFechaPublicacion() {
 		return fechaPublicacion;
 	}
 
-	public void setFechaPublicacion(Date fechaPublicacion) {
+
+	public void setFechaPublicacion(String fechaPublicacion) {
 		this.fechaPublicacion = fechaPublicacion;
 	}
 
-	public TipoArticulo getTipoArt() {
+
+	public String getTipoArt() {
 		return tipoArt;
 	}
 
-	public void setTipoArt(TipoArticulo tipoArt) {
+
+	public void setTipoArt(String tipoArt) {
 		this.tipoArt = tipoArt;
 	}
+
 
 	public String getRutaArchivoArt() {
 		return rutaArchivoArt;
 	}
 
+
 	public void setRutaArchivoArt(String rutaArchivoArt) {
 		this.rutaArchivoArt = rutaArchivoArt;
 	}
+	
+
+
+	public int getCantLikes() {
+		return cantLikes;
+	}
+
+
+	public void setCantLikes(int cantLikes) {
+		this.cantLikes = cantLikes;
+	}
+	
+
+
+	public String getRutaFotoPerfil() {
+		return rutaFotoPerfil;
+	}
+
+
+	public void setRutaFotoPerfil(String rutaFotoPerfil) {
+		this.rutaFotoPerfil = rutaFotoPerfil;
+	}
+
 
 	@Override
 	public String toString() {
-		return "Articulo [id=" + id + ", titulo=" + titulo + ", autor=" + autor + ", fechaPublicacion="
+		return "Articulo [idArt=" + idArt + ", titulo=" + titulo + ", autor=" + autor + ", fechaPublicacion="
 				+ fechaPublicacion + ", tipoArt=" + tipoArt + ", rutaArchivoArt=" + rutaArchivoArt + "]";
 	}
+	
+	
 	
 }
--- RevistaModa/src/revistaModa/clases/Usuario.java
+++ RevistaModa/src/revistaModa/clases/Usuario.java
@@ -4,64 +4,54 @@ public class Usuario {
 	private int idUsu;
 	private String username;
 	private String contrasenya;
-	private String nombreUsu;
-	private String apellidoUsu;
 	private String email;
-	private int tlf;
-	public Usuario(int idUsu, String username, String contrasenya, String nombreUsu, String apellidoUsu, String email,
-			int tlf) {
+	
+	public Usuario(int idUsu, String username, String contrasenya, String email) {
 		super();
 		this.idUsu = idUsu;
 		this.username = username;
 		this.contrasenya = contrasenya;
-		this.nombreUsu = nombreUsu;
-		this.apellidoUsu = apellidoUsu;
 		this.email = email;
-		this.tlf = tlf;
 	}
-	
+
 	public int getIdUsu() {
 		return idUsu;
 	}
+
 	public void setIdUsu(int idUsu) {
 		this.idUsu = idUsu;
 	}
+
 	public String getUsername() {
 		return username;
 	}
+
 	public void setUsername(String username) {
 		this.username = username;
 	}
+
 	public String getContrasenya() {
 		return contrasenya;
 	}
+
 	public void setContrasenya(String contrasenya) {
 		this.contrasenya = contrasenya;
 	}
-	public String getNombreUsu() {
-		return nombreUsu;
-	}
-	public void setNombreUsu(String nombreUsu) {
-		this.nombreUsu = nombreUsu;
-	}
-	public String getApellidoUsu() {
-		return apellidoUsu;
-	}
-	public void setApellidoUsu(String apellidoUsu) {
-		this.apellidoUsu = apellidoUsu;
-	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
-	public int getTlf() {
-		return tlf;
+
+	@Override
+	public String toString() {
+		return "Usuario [idUsu=" + idUsu + ", username=" + username + ", contrasenya=" + contrasenya + ", email="
+				+ email + "]";
 	}
-	public void setTlf(int tlf) {
-		this.tlf = tlf;
-	}
+
 	
 	
 	
