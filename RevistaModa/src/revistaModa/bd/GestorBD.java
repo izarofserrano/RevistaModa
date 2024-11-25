0872521a67f5fa25ec0950953b4e247f8e790a26
izarofserrano <i.serrano@opendeusto.es>
2024-11-25 15:32:31 +0100

    Creación de la clase BD y algunos métodos

---
+++ RevistaModa/src/revistaModa/bd/GestorBD.java
@@ -0,0 +1,36 @@
+package revistaModa.bd;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class GestorBD {
+	private static Connection con;
+
+	public static void initBD(String nombreBD)  {
+		con = null;
+
+		try {
+			Class.forName("org.sqlite.JDBC");
+			con = DriverManager.getConnection("jdbc:sqlite:" + nombreBD);
+
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void closeBD() {
+		if (con != null) {
+			try {
+				con.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+
+
+}
