85b07525124be08a09eeaf8082b3603331baaa7d
Génesis Balcazar <g.balcazar@opendeusto.es>
2024-12-05 16:14:45 +0100

    Error de tablaEstadisticas resuelto

--- RevistaModa/src/revistaModa/gui/ModeloEstadisticas.java
+++ RevistaModa/src/revistaModa/gui/ModeloEstadisticas.java
@@ -68,7 +68,7 @@ public class ModeloEstadisticas extends DefaultTableModel{
 		switch(column){
 		case 0:return art.getTitulo();
 		case 1:return art.getAutor();
-		case 2:return art.getlFotos().get(row).getRutaFoto();
+		case 2:return art.getlFotos().get(0).getRutaFoto();
 		case 3:return art.getLikesCount();
 		case 4:return (RevistaModa.valoracionMedia(art)) ;
 		default:return null;
